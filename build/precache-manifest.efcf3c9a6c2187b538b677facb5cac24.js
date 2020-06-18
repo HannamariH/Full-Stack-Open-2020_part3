93078a569f27af04c5c060b9700770de3c07c305
Hannamari <hannamari.heiniluoma@gmail.com>
2020-06-18 11:51:18 +0300

    build lisätty

---
+++ build/precache-manifest.efcf3c9a6c2187b538b677facb5cac24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444a8a15eb3700172a2d70e0fa20a9b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0e8d8e4126f98a8962",
+    "url": "/static/css/main.2202aaa8.chunk.css"
+  },
+  {
+    "revision": "53fe9890aa9b55c92f05",
+    "url": "/static/js/2.6b41a6c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b41a6c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0e8d8e4126f98a8962",
+    "url": "/static/js/main.7bbc7c2c.chunk.js"
+  },
+  {
+    "revision": "7fcbd9239da9c0ee99db",
+    "url": "/static/js/runtime-main.53fdc1a5.js"
+  }
+]);
