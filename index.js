8c25340dbb00d24a14b7d50de5f82ffcbc3cbf14
Hannamari <hannamari.heiniluoma@gmail.com>
2020-06-18 15:39:41 +0300

    poistettu notes

--- index.js
+++ index.js
@@ -46,27 +46,6 @@ let persons = [
   },
 ]
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2020-01-10T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2020-01-10T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2020-01-10T19:20:14.298Z",
-    important: true
-  }
-]
-
 app.get("/info", (req, res) => {
   res.send(
     `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
@@ -75,10 +54,6 @@ app.get("/info", (req, res) => {
 
 app.get("/api/persons", (req, res) => {
   res.json(persons)
-})
-
-app.get("/api/notes", (req, res) => {
-  res.json(notes)
 })
 
 app.get("/api/persons/:id", (request, response) => {
